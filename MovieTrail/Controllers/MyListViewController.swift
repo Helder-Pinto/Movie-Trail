1eb4782c435b3dec09cb183dcb7af8138a73dc56
Helder Pinto <hybridhp@hotmail.com>
2019-04-06 18:14:30 +0200

    core data

--- MovieTrail/Controllers/MyListViewController.swift
+++ MovieTrail/Controllers/MyListViewController.swift
@@ -7,18 +7,26 @@
 //
 
 import UIKit
+import CoreData
 
 class MyListViewController: UITableViewController {
     
+     var myMoviesArray = [Item]()
+     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
-    var myMoviesArray = ["Us", "Wolf Of WallStreet", "Blood Diamonds"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-      
+        
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
+        loadItems()
     }
-
+    
+    
+    
+    
+    
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -26,10 +34,25 @@ class MyListViewController: UITableViewController {
         return myMoviesArray.count
     }
     
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyListCell", for: indexPath)
-        cell.textLabel?.text = myMoviesArray[indexPath.row]
+        
+        let item = myMoviesArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.watched ? .checkmark : .none
+        
+//        if item.watched == true {
+//            cell.accessoryType = .checkmark
+//
+//        } else {
+//            cell.accessoryType = .none
+//        }
+        
+        
         return cell
     }
 
@@ -37,8 +60,14 @@ class MyListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-       // if tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark ? .none : .checkmark
-       
+         // remove items
+//         context.delete(myMoviesArray[indexPath.row])
+//         myMoviesArray.remove(at: indexPath.row)
+        
+        
+        myMoviesArray[indexPath.row].watched = !myMoviesArray[indexPath.row].watched
+        
+        saveItems()
         
         tableView.deselectRow(at:indexPath, animated: true)
         
@@ -54,10 +83,18 @@ class MyListViewController: UITableViewController {
         let alert = UIAlertController(title: "add new movie", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add item", style: .default) { (action) in
-           
-            self.myMoviesArray.append(textField.text!)
             
-            self.tableView.reloadData()
+           
+            
+           
+            let newItem = Item(context: self.context)
+            
+            newItem.title = textField.text!
+            newItem.watched = false
+            
+            self.myMoviesArray.append(newItem)
+            
+             self.saveItems()
         }
         
         alert.addTextField { (alertTextField) in
@@ -70,7 +107,53 @@ class MyListViewController: UITableViewController {
         alert.addAction(action)
         present(alert, animated:true, completion: nil)
         
+       
+        
         
     }
     
+    
+    //MARK: Core Data Saving support
+    
+    func saveItems(){
+        
+        do{
+            try context.save()
+        } catch{
+            print("Erro saving context \(error)")
+            
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func loadItems (with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+        
+        // let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        do{
+            myMoviesArray =   try   context.fetch(request)
+            
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        
+    }
 }
+
+
+
+//MARK: Search
+
+extension  MyListViewController : UISearchBarDelegate {
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+    }
+    
+    
+    
+    
+}
+
